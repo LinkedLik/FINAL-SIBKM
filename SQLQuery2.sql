4dc0f4157480d5a3e8fe1f889516085773a714db
Daniel <Daniel@gmail.com>
2024-06-07 16:07:04 +0700

    Test merge commit

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -7,3 +7,8 @@ BEGIN
     SELECT d.id, 'Resign', GETDATE()
     FROM deleted d;
 END;
+CREATE VIEW tes_view
+SELECT tbl_countries_1.*, dbo.tbl_account.*, dbo.tbl_account.username AS Expr1, dbo.tbl_account.is_expired AS Expr2
+FROM     dbo.tbl_countries AS tbl_countries_1 CROSS JOIN
+                  dbo.tbl_account INNER JOIN
+                  dbo.tbl_account_roles ON dbo.tbl_account.id = dbo.tbl_account_roles.account
