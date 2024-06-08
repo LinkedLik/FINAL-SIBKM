1370a2bcd5b61169feb4d8be21d33fc78f795b21
Daniel <Daniel@gmail.com>
2024-06-08 14:20:24 +0700

    Function Otp Match

---
+++ func_otp_match.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION func_otp_match (@otp NVARCHAR(6), @storedOtp NVARCHAR(6))
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @result BIT;
+
+    IF (@otp = @storedOtp)
+        SET @result = 1;
+    ELSE
+        SET @result = 0;
+
+    RETURN @result;
+END;
+
+SELECT [dbo].[func_otp_match]('123456', '123456') AS IsMatch;
